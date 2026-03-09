4f9c935feb7037fd03afd670d4f2f6cd7bb554a9
naemshaikh <156431865+naemshaikh@users.noreply.github.com>
2026-03-09 23:10:04 +0530

    Increase Gunicorn configuration parameters

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,11 +1,10 @@
 import os
-
 bind             = "0.0.0.0:" + os.environ.get("PORT", "10000")
 workers          = 1
-threads          = 2
+threads          = 4
 worker_class     = "gthread"
-timeout          = 120
+timeout          = 300
 keepalive        = 5
 preload_app      = False
 loglevel         = "info"
-graceful_timeout = 30
+graceful_timeout = 60
